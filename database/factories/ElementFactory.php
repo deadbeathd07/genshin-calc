529b9a06ab7b5514b4b00310a2ac7b3291ac627b
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-02 17:51:30 +0300

    add factories for character models, constellations, elements, character rankings, teams

---
+++ database/factories/ElementFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Element;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\element>
+ */
+class ElementFactory extends Factory
+{
+  protected $model = Element::class;
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      //
+    ];
+  }
+}
---
+++ database/factories/RatingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Rating;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rating>
+ */
+class RatingFactory extends Factory
+{
+  protected $model = Rating::class;
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      //
+    ];
+  }
+}
