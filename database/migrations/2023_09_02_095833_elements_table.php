b895762043e4c372a3d3614911f6f40ac5e04536
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-02 13:12:26 +0300

    added migrations to create leaderboards, elements, constellations, characters and teams

---
+++ database/migrations/2023_09_02_095833_elements_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('elements_table', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('img_path');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('elements_table');
+  }
+};
