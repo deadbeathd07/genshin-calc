c5f6003167b3df61a4ae09bb7f24bbe8937bf900
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-12 15:40:08 +0300

    entered the initial data on the characters

--- database/migrations/2023_09_09_122626_create_characters_table.php
+++ database/migrations/2023_09_09_122626_create_characters_table.php
@@ -28,14 +28,14 @@ return new class extends Migration
         'name' => 'Al-Khaytam',
         'element_id' => 4,
         'dps' => true,
-        'sub_dps' => false,
-        'support' => false,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/al_khaytam.webp'
       ],
       [
         'name' => 'Albedo',
         'element_id' => 2,
-        'dps' => false,
+        'dps' => null,
         'sub_dps' => true,
         'support' => true,
         'img_path' => '/public/images/heroes/albedo.webp'
@@ -44,24 +44,24 @@ return new class extends Migration
         'name' => 'Aloy',
         'element_id' => 6,
         'dps' => true,
-        'sub_dps' => false,
-        'support' => false,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/aloy.webp'
       ],
       [
         'name' => 'Amber',
         'element_id' => 7,
-        'dps' => false,
+        'dps' => null,
         'sub_dps' => true,
-        'support' => false,
+        'support' => null,
         'img_path' => '/public/images/heroes/amber.webp'
       ],
       [
         'name' => 'Ayaka',
         'element_id' => 6,
         'dps' => true,
-        'sub_dps' => false,
-        'support' => false,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/ayaka.webp'
       ],
       [
@@ -76,7 +76,7 @@ return new class extends Migration
         'name' => 'Bai Dou',
         'dps' => true,
         'sub_dps' => true,
-        'support' => false,
+        'support' => null,
         'element_id' => 3,
         'img_path' => '/public/images/heroes/baidou.webp'
       ],
@@ -84,30 +84,30 @@ return new class extends Migration
         'name' => 'Barbara',
         'element_id' => 5,
         'dps' => true,
-        'sub_dps' => false,
+        'sub_dps' => null,
         'support' => true,
         'img_path' => '/public/images/heroes/barbara.webp'
       ],
       [
         'name' => 'Bay Chzhu',
         'element_id' => 4,
-        'dps' => false,
-        'sub_dps' => false,
+        'dps' => null,
+        'sub_dps' => null,
         'support' => true,
         'img_path' => '/public/images/heroes/bay_chzhu.webp'
       ],
       [
         'name' => 'Bennet',
         'element_id' => 7,
-        'dps' => false,
-        'sub_dps' => false,
+        'dps' => null,
+        'sub_dps' => null,
         'support' => true,
         'img_path' => '/public/images/heroes/bennett.webp'
       ],
       [
         'name' => 'Candace',
-        'dps' => false,
-        'sub_dps' => false,
+        'dps' => null,
+        'sub_dps' => null,
         'support' => true,
         'element_id' => 5,
         'img_path' => '/public/images/heroes/candace.webp'
@@ -123,335 +123,497 @@ return new class extends Migration
       [
         'name' => 'Collei',
         'element_id' => 4,
-        'dps' => false,
+        'dps' => null,
         'sub_dps' => true,
-        'support' => false,
+        'support' => null,
         'img_path' => '/public/images/heroes/collei.webp'
       ],
       [
         'name' => 'Cyno',
         'element_id' => 3,
         'dps' => true,
-        'sub_dps' => false,
-        'support' => false,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/cyno.webp'
       ],
       [
         'name' => 'Dehya',
         'element_id' => 7,
-        'dps' => false,
-        'sub_dps' => false,
-        'support' => false,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/dehya.webp'
       ],
       [
         'name' => 'Diluc',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/diluc.webp'
       ],
       [
         'name' => 'Diona',
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'element_id' => 6,
         'img_path' => '/public/images/heroes/diona.webp'
       ],
       [
         'name' => 'Dori',
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'element_id' => 3,
         'img_path' => '/public/images/heroes/dori.webp'
       ],
       [
         'name' => 'Eula',
         'element_id' => 6,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/eula.webp'
       ],
       [
         'name' => 'Faruzan',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/faruzan.webp'
       ],
       [
         'name' => 'Fischl',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/fischl.webp'
       ],
       [
         'name' => 'Fremine',
         'element_id' => 6,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/fremine.webp'
       ],
       [
         'name' => 'Gan Yu',
         'element_id' => 6,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/ganyu.webp'
       ],
       [
         'name' => 'Gorou',
         'element_id' => 2,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/gorou.webp'
       ],
       [
         'name' => 'Heizou',
         'element_id' => 1,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/heizou.png'
       ],
       [
         'name' => 'Hu Tao',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/hutao.webp'
       ],
       [
         'name' => 'Itto',
         'element_id' => 2,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/itto.webp'
       ],
       [
         'name' => 'Jean',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/jean.webp'
       ],
       [
         'name' => 'Kaeya',
         'element_id' => 6,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/kaeya.webp'
       ],
       [
         'name' => 'Kavekh',
         'element_id' => 4,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/kavekh.webp'
       ],
       [
         'name' => 'Kazuha',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/kazuha.webp'
       ],
       [
         'name' => 'Ke Qing',
         'element_id' => 3,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/keqing.webp'
       ],
       [
         'name' => 'Kirara',
         'element_id' => 4,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/kirara.webp'
       ],
       [
         'name' => 'Klee',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/klee.webp'
       ],
       [
         'name' => 'Kokonomi',
         'element_id' => 5,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/kokomi.webp'
       ],
       [
         'name' => 'Layla',
         'element_id' => 6,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/layla.webp'
       ],
       [
         'name' => 'Linett',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/linett.webp'
       ],
       [
         'name' => 'Lini',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/lini.webp'
       ],
       [
         'name' => 'Lisa',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/lisa.webp'
       ],
       [
         'name' => 'Mika',
         'element_id' => 6,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/mika.webp'
       ],
       [
         'name' => 'Mona',
         'element_id' => 5,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/mona.webp'
       ],
       [
         'name' => 'Nahida',
         'element_id' => 4,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/nahida.webp'
       ],
       [
         'name' => 'Nilou',
         'element_id' => 5,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/nilou.webp'
       ],
       [
         'name' => 'Ning Guang',
         'element_id' => 2,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/ningguang.webp'
       ],
       [
         'name' => 'Noelle',
         'element_id' => 2,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/noelle.webp'
       ],
       [
         'name' => 'Qi Qi',
         'element_id' => 6,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/qiqi.webp'
       ],
       [
         'name' => 'Raiden',
         'element_id' => 3,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/raiden.webp'
       ],
       [
         'name' => 'Razor',
         'element_id' => 3,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/razor.webp'
       ],
       [
         'name' => 'Rosaria',
         'element_id' => 6,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/rosaria.webp'
       ],
       [
         'name' => 'Sara',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/sara.webp'
       ],
       [
         'name' => 'Sayu',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/sayu.webp'
       ],
       [
         'name' => 'Scaramouche',
         'element_id' => 1,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/scaramouche.webp'
       ],
       [
         'name' => 'Shen Khe',
         'element_id' => 6,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/shen_khe.png'
       ],
       [
         'name' => 'Sinobu',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/sinobu.webp'
       ],
       [
         'name' => 'Sucrose',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/sucrose.webp'
       ],
       [
         'name' => 'Tartaglia',
         'element_id' => 5,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/tartaglia.webp'
       ],
       [
         'name' => 'Thoma',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/thoma.webp'
       ],
       [
         'name' => 'Tighnari',
         'element_id' => 4,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/tighnari.webp'
       ],
       [
         'name' => 'Traveler',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/traveler.webp'
       ],
       [
         'name' => 'Traveler',
         'element_id' => 2,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/traveler.webp'
       ],
       [
         'name' => 'Traveler',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/traveler.webp'
       ],
       [
         'name' => 'Traveler',
         'element_id' => 4,
-        'img_path' => '/public/images/heroes/traveler.webp'
-      ],
-      [
-        'name' => 'Traveler',
-        'element_id' => 5,
-        'img_path' => '/public/images/heroes/traveler.webp'
-      ],
-      [
-        'name' => 'Traveler',
-        'element_id' => 6,
-        'img_path' => '/public/images/heroes/traveler.webp'
-      ],
-      [
-        'name' => 'Traveler',
-        'element_id' => 7,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/traveler.webp'
       ],
       [
         'name' => 'Venti',
         'element_id' => 1,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/venti.webp'
       ],
       [
         'name' => 'Xiang Ling',
         'element_id' => 7,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/xiangling.webp'
       ],
       [
         'name' => 'Xiao',
         'element_id' => 1,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/xiao.webp'
       ],
       [
         'name' => 'Xing Qiu',
         'element_id' => 5,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/xingqiu.webp'
       ],
       [
         'name' => 'Xin Yan',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/xinyan.webp'
       ],
       [
         'name' => 'Yae Miko',
         'element_id' => 3,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => null,
         'img_path' => '/public/images/heroes/yae_miko.png'
       ],
       [
         'name' => 'Yan Fei',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/yanfei.webp'
       ],
       [
         'name' => 'Yao Yao',
         'element_id' => 4,
+        'dps' => null,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/yao_yao.webp'
       ],
       [
         'name' => 'Ye Lan',
         'element_id' => 5,
+        'dps' => true,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/yelan.webp'
       ],
       [
         'name' => 'Yoimiya',
         'element_id' => 7,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => null,
         'img_path' => '/public/images/heroes/yoimiya.webp'
       ],
       [
         'name' => 'Yun Tszin',
         'element_id' => 2,
+        'dps' => true,
+        'sub_dps' => null,
+        'support' => true,
         'img_path' => '/public/images/heroes/yun_tszin.png'
       ],
       [
         'name' => 'Zhong Li',
         'element_id' => 2,
+        'dps' => null,
+        'sub_dps' => true,
+        'support' => true,
         'img_path' => '/public/images/heroes/zhongli.webp'
       ],
     );
