ae7b416cabcfa2b0a59ca0af5f934c3628bf11ac
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-21 16:30:24 +0300

    pages have been implemented into the router

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,26 +1,37 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import Default from "@/layouts/default/Default.vue";
+import Home from "@/views/Home.vue";
+import Register from "@/views/Register.vue";
+import Entry from "@/views/Entry.vue";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: Default,
     children: [
       {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: Register,
+      },
+      {
+        path: "entry",
+        name: "Entry",
+        component: Entry,
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
