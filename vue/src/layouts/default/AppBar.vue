8c5d2d1cf3b1d723a942c2a3e992f50d2cbd92a8
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-21 15:54:40 +0300

    minimal functionality has been added for the header component

---
+++ vue/src/layouts/default/AppBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app-bar title="Calculator">
+    <template v-slot:append>
+      <v-btn icon="mdi-account-circle" />
+    </template>
+  </v-app-bar>
+</template>
+<script></script>
+
+<style></style>
