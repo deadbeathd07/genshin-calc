1c01b1e5ee98339979d99fb7cfbc051fac6c98cc
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-21 18:37:46 +0300

    form has been created

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,5 +1,49 @@
 <template>
-  <h1>Register</h1>
+  <v-sheet class="pa-3 d-flex justify-center align-center" color="transparent">
+    <v-form @submit.prevent="onSubmit">
+      <div class="mb-5">
+        <h2 class="text-center mb-2">Sign Up</h2>
+        <div class="d-flex align-center">
+          <p class="mr-2">Already have an account?</p>
+          <v-btn
+            :to="{ name: 'Entry' }"
+            text="Log In"
+            variant="text"
+            color="yellow"
+          />
+        </div>
+      </div>
+      <v-text-field
+        v-model="formRegistration.name"
+        label="Name"
+        class="w-100"
+      />
+      <v-text-field
+        v-model="formRegistration.email"
+        label="E-mail"
+        class="w-100"
+        type="email"
+      />
+      <v-text-field
+        v-model="formRegistration.password"
+        label="Password"
+        class="w-100"
+        type="password"
+      />
+      <v-btn type="submit" text="Submit" class="w-100" />
+    </v-form>
+  </v-sheet>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const formRegistration = ref(store.state.formRegistration);
+
+function onSubmit() {
+  store.dispatch("setFormRegistration", JSON.stringify(formRegistration.value));
+}
+</script>
 <style></style>
