44a8259528b5f1fc9983af2f2e01bfb114468eb7
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-21 16:10:24 +0300

    tooltip has been added

--- vue/src/layouts/default/AppBar.vue
+++ vue/src/layouts/default/AppBar.vue
@@ -1,10 +1,19 @@
 <template>
   <v-app-bar title="Calculator">
     <template v-slot:append>
-      <v-btn icon="mdi-account-circle" />
+      <v-btn type="text">Sign Up</v-btn>
+      <v-tooltip text="Sign In">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" icon="mdi-account-circle" />
+        </template>
+      </v-tooltip>
     </template>
   </v-app-bar>
 </template>
 <script></script>
 
-<style></style>
+<style>
+.gc {
+  text-transform: uppercase;
+}
+</style>
