96305bad556c40680555fa367a17dab790f8ade5
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-21 16:32:59 +0300

    transitions to other pages have been added

--- vue/src/layouts/default/AppBar.vue
+++ vue/src/layouts/default/AppBar.vue
@@ -1,15 +1,38 @@
 <template>
-  <v-app-bar title="Calculator">
+  <v-app-bar>
+    <template v-slot:prepend>
+      <v-btn
+        size="x-large"
+        type="text"
+        text="Calculator"
+        @click="toPage('Home')"
+      />
+    </template>
     <template v-slot:append>
-      <v-btn type="text">Sign Up</v-btn>
+      <v-btn size="large" type="text" @click="toPage('Register')">
+        Sign Up
+      </v-btn>
       <v-tooltip text="Sign In">
         <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" icon="mdi-account-circle" />
+          <v-btn
+            size="large"
+            v-bind="props"
+            icon="mdi-account-circle"
+            @click="toPage('Entry')"
+          />
         </template>
       </v-tooltip>
     </template>
   </v-app-bar>
 </template>
-<script></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function toPage(name) {
+  router.push({ name: name });
+}
+</script>
 
 <style></style>
