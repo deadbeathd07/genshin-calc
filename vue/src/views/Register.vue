8ac571d968ee32f278b4ceb95c334c7be6e181c5
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 13:01:17 +0300

    the form was imported and switched on

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,40 +1,10 @@
 <template>
   <v-sheet class="pa-3 d-flex justify-center align-center" color="transparent">
-    <v-form @submit.prevent="onSubmit">
-      <div class="mb-5">
-        <h2 class="text-center mb-2">Sign Up</h2>
-        <div class="d-flex align-center">
-          <p class="mr-2">Already have an account?</p>
-          <v-btn
-            :to="{ name: 'Entry' }"
-            text="Log In"
-            variant="text"
-            color="cyan-darken-2"
-          />
-        </div>
-      </div>
-      <v-text-field
-        v-model="formRegistration.name"
-        label="Name"
-        class="w-100"
-      />
-      <v-text-field
-        v-model="formRegistration.email"
-        label="E-mail"
-        class="w-100"
-        type="email"
-      />
-      <v-text-field
-        v-model="formRegistration.password"
-        label="Password"
-        class="w-100"
-        type="password"
-      />
-      <v-btn type="submit" text="Submit" class="w-100" color="cyan-darken-1" />
-    </v-form>
+    <default-form @on-submit="onSubmit" :form="formRegistration" />
   </v-sheet>
 </template>
 <script setup>
+import DefaultForm from "@/components/AppForm.vue";
 import { ref } from "vue";
 import { useStore } from "vuex";
 
