4e27fe5220fab7880c6048f5b11b30168071ee06
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 13:31:30 +0300

    added a property for the login form to the repository, added corresponding actions and mutations

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { createStore } from "vuex";
 
 const store = createStore({
@@ -7,6 +7,10 @@ const store = createStore({
       formRegistration: {
         name: null,
         email: null,
+        password: null,
+      },
+      formEntry: {
+        name: null,
         password: null,
       },
       characters: null,
@@ -19,6 +23,9 @@ const store = createStore({
     SET_FORM_REGISTRATION(state, newFormRegistration) {
       state.formRegistration = newFormRegistration;
     },
+    SET_FORM_ENTRY(state, newFormEntry) {
+      state.formEntry = newFormEntry;
+    },
   },
   actions: {
     getCharacters({ commit }, newCharacters) {
@@ -27,7 +34,12 @@ const store = createStore({
     setFormRegistration({ commit }, newFormRegistration) {
       let formRegistration = JSON.parse(newFormRegistration);
       commit("SET_FORM_REGISTRATION", formRegistration);
-      axios.post("http://127.0.0.1:8000/api/registration", formRegistration);
+      // axios.post("http://127.0.0.1:8000/api/registration", formRegistration);
+    },
+    setFormEntry({ commit }, newFormEntry) {
+      let formEntry = JSON.parse(newFormEntry);
+      commit("SET_FORM_ENTRY", formEntry);
+      // axios.post("http://127.0.0.1:8000/api/entry", formEntry);
     },
   },
 });
