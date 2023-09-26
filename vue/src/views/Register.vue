c32b87152d3000f1475741dd6c5984a478c1f828
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 13:08:55 +0300

    title object has been generated

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,6 +1,10 @@
 <template>
   <v-sheet class="pa-3 d-flex justify-center align-center" color="transparent">
-    <default-form @on-submit="onSubmit" :form="formRegistration" />
+    <default-form
+      @on-submit="onSubmit"
+      :form="formRegistration"
+      :title="formTitle"
+    />
   </v-sheet>
 </template>
 <script setup>
@@ -11,6 +15,13 @@ import { useStore } from "vuex";
 const store = useStore();
 
 const formRegistration = ref(store.state.formRegistration);
+
+const formTitle = {
+  title: "Sign Up",
+  question: "Already have an account?",
+  link: { name: "Entry" },
+  btn_text: "Log In",
+};
 
 function onSubmit() {
   store.dispatch("setFormRegistration", JSON.stringify(formRegistration.value));
