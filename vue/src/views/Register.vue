ce0f18d4515ea8d3d54178ebc6c9eb55c66a4e0a
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 13:22:27 +0300

    property changed

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -2,19 +2,19 @@
   <v-sheet class="pa-3 d-flex justify-center align-center" color="transparent">
     <default-form
       @on-submit="onSubmit"
-      :form="formRegistration"
+      :initial-form="formRegistration"
       :is-entry="false"
     />
   </v-sheet>
 </template>
 <script setup>
 import DefaultForm from "@/components/AppForm.vue";
-import { ref } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
 
-const formRegistration = ref(store.state.formRegistration);
+const formRegistration = computed(() => store.state.formRegistration);
 
 function onSubmit() {
   store.dispatch("setFormRegistration", JSON.stringify(formRegistration.value));
