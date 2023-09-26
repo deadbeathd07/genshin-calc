31420c159ce1f4104f921079162308a803cdaaa8
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 12:59:30 +0300

    the form was transferred to the component, props and emits were registered

---
+++ vue/src/components/AppForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-form @submit.prevent="emits['onSubmit']">
+    <div class="mb-5">
+      <h2 class="text-center mb-2">Sign Up</h2>
+      <div class="d-flex align-center">
+        <p class="mr-2">Already have an account?</p>
+        <v-btn
+          :to="{ name: 'Entry' }"
+          text="Log In"
+          variant="text"
+          color="cyan-darken-2"
+        />
+      </div>
+    </div>
+    <v-text-field v-model="props.form.name" label="Name" class="w-100" />
+    <v-text-field
+      v-model="props.form.email"
+      label="E-mail"
+      class="w-100"
+      type="email"
+    />
+    <v-text-field
+      v-model="props.form.password"
+      label="Password"
+      class="w-100"
+      type="password"
+    />
+    <v-btn type="submit" text="Submit" class="w-100" color="cyan-darken-1" />
+  </v-form>
+</template>
+
+<script setup>
+const props = defineProps({
+  form: Object,
+});
+const emits = defineEmits(["onSubmit"]);
+</script>
+
+<style></style>
