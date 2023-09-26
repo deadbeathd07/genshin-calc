359be26a735ef37112a8b6dc6d0ed9cf868b4ef0
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 13:32:28 +0300

    added login form to the page

--- vue/src/views/Entry.vue
+++ vue/src/views/Entry.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>Entry</h1>
+  <v-sheet class="pa-3 d-flex justify-center align-center" color="transparent">
+    <default-form
+      @on-submit="onSubmit"
+      :initial-form="formEntry"
+      :is-entry="true"
+    />
+  </v-sheet>
 </template>
-<script setup></script>
+<script setup>
+import DefaultForm from "@/components/AppForm.vue";
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const formEntry = computed(() => store.state.formEntry);
+
+function onSubmit() {
+  store.dispatch("setFormEntry", JSON.stringify(formEntry.value));
+}
+</script>
 <style></style>
