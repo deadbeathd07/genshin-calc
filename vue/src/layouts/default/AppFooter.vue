e28412cc762dc82fb957d56545de82f3b614cbfa
deadbeathd07 <deadbeat.hd.07@gmail.com>
2023-09-26 11:48:00 +0300

    footer component added

---
+++ vue/src/layouts/default/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer>
+    <a
+      target="_blank"
+      href="https://icons8.com/icon/UMjPnpMna57l/genshin-impact"
+    >
+      Genshin Impact
+    </a>
+    icon by
+    <a target="_blank" href="https://icons8.com">Icons8</a>
+  </v-footer>
+</template>
+<script setup></script>
+<style></style>
